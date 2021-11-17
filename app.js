d6704f568df25e290b66959f07903d0a15999e29
josh manansala <joshmanansalagsd@gmail.com>
2021-11-16 18:52:46 -0800

    logic set up

---
+++ app.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Default Variables 
+    const grid = document.querySelector('.grid')
+    let square = Array.from(document.querySelector('.grid div'))
+    const scoreDisplay = document.querySelector('#score')
+    const startBtn = document.querySelector('#start-button')
+    const width = 10
+    let nextRandom = 0
+    let timerId
+    let score = 0
+    const colors = [
+        'orange',
+        'red',
+        'purple',
+        'green',
+        'blue'
+    ]
+
+    // The Tetrominoes
+
+    // Randomly Select a Tetrominoes and its first location 
+
+    // draw the Tetromino 
+
+    // undraw the Tetromino
+
+    // assign functions to keyCodes
+
+    // move down function 
+
+    // freeze function + start a new tetromino falling
+
+    //move the tetromino left, unless is at the edge or there is a block
+
+    // move the tetromino right, unless is at the edge or there is a block 
+
+    // FIX ROTATION OF TETROMINOS AT THE EDGE
+
+    // Rotate the tetromino
+
+    // show up-next tetromino in mini-grid display
+
+    // the Tetrominos without rotations
+
+    // display the shape int he mini-grid display
+
+    // add functionality to the button
+
+    // add score
+    
+    // game over 
+})
